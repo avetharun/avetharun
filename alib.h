e1aadb3904fbfc8ef9b39cf169292c87961264db
Lillium <avetharun@lazyne.co>
2022-06-29 12:13:31 -0700

    Create alib.h

---
+++ alib.h
@@ -0,0 +1,37 @@
+
+//  ALIB: A C-Style programming utility header for tedious tasks.
+//
+//  Any instance of array creation or object creation is handled using the C-style malloc()/free() functions!
+//  
+//  Note: this is the C version! It won't have some things such as std::string utilities, so make sure to use https://github.com/avetharun/avetharun/alib.hpp!
+//
+
+
+
+/** - https://github.com/avetharun/avetharun/blob/main/ave_libs.hpp
+ @copyright Avetharun 2021-2022 - GNU-GPL3-a
+ Let's keep this simple. It follows the GNU GPL3 license, with very few modifications and exceptions.
+ @ You can:
+  - Use this commercially
+  - Distribute modified versions
+  - Modify this file and/or projects
+  - Patent projects that use this file
+  - Use this file and/or project privately, ie. for use in an internal server (See limitations & Exceptions)
+  - Use freely under the following conditions
+ @ Conditions:
+  - You must disclose the source file and/or author when distributing this file
+  - A copy of this license must be attached to the file, in verbatim
+  - The same (or similar) license must be used if you modify and license your version
+  - Changes must be listed. ie. modifying source code must have your name and change in the file.
+    - To follow this it must either be
+      A: Beside the change (on top or on bottom, in a comment)
+      B: In the AUTHORs section of the file/project
+      C: in any changelog that references the file's changes
+ @ Limitations / What_you_cannot_do
+  - The user (you) are not liable for the harmful actions this program or file may cause, if any.
+  - Keep code closed source (*See exceptions)
+ @b Exceptions
+  - If code is closed source, it must be in a private setting. Examples are as follows:
+    EXA: A server used to host and/or distribute files
+    EXB: Used as a base for porting and/or in microcontrollers
+ */
